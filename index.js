03199e0cfadac70ddaf251310c737133d0d5177c
christian <cpalvetzian@gmail.com>
2019-05-15 21:35:07 -0400

    broken OBJLoader is not a constructor

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 7777
+
+app.use(express.static('/public'));
+
+app.listen(port, () => console.log(`Blender/three.js listening on port ${port}!`))
